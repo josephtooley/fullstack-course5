9f067fcd21476f082fb79ed63073d831763409c2
Joseph Tooley <joseph.tooley@gmail.com>
2016-12-11 20:46:24 -0500

    module1 working

---
+++ module1-solution/app.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = [ '$scope', '$filter' ];
+  function LunchCheckController($scope, $filter) {
+    $scope.sayMessage = function () {
+      var input = $scope.menuTextBox;
+      if(input == null) {
+        $scope.message = "Please enter data first"
+      } else {
+        var arrayLength = input.split(',').length;
+        switch(true) {
+          case (arrayLength <= 3):
+            $scope.message = "Enjoy!";
+            break;
+          case (arrayLength > 3):
+            $scope.message = "Too much!";
+            break;
+        }
+      }
+    }
+  }
+})();
