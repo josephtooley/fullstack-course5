ed120a55a039eb86713f6022496a11c733263e57
Joseph Tooley <joseph.tooley@gmail.com>
2017-01-01 16:03:38 -0500

    Search hard-coded, but working

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,32 +3,40 @@
 
   angular.module('NarrowItDownApp', [])
   .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService);
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemsDirective);
 
-  MenuSearchService.$inject = [ '$http' ];
-  function MenuSearchService($http) {
+  MenuSearchService.$inject = [ '$http', '$filter' ];
+  function MenuSearchService($http, $filter) {
     var service = this;
 
     service.getMatchedMenuItems = function () {
-      var response = $http({
+      return $http({
         method: "GET",
         url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+      }).then(function (result) {
+        var menuItems = result.data.menu_items
+        var foundItems = $filter('filter')(menuItems, { description: "carrots" } );
+        return foundItems;
       });
-      return response;
     };
+  }
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html'
+    };
+
+    return ddo;
   }
 
   NarrowItDownController.$inject = [ 'MenuSearchService' ];
   function NarrowItDownController(MenuSearchService) {
     var narrowDown = this;
-
     var promise = MenuSearchService.getMatchedMenuItems();
 
     promise.then(function (response) {
-      narrowDown.found = response.data;
-    })
-    .catch(function (error) {
-      console.log("Something went wrong");
+      narrowDown.found = response;
     });
   }
 
